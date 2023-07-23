a42a538b96f68d0183a6a9bf53da5acf01a4bf8b
manielsen13 <marshall.niels@gmail.com>
2023-07-23 16:16:13 -0400

    Add alts for images

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logoImg from "../assets/logo.png";
 function Logo() {
   return (
     <div className="flex flex-col items-center self-center gap-2 select-none">
-      <img src={logoImg} className="w-20 h-auto"></img>
+      <img src={logoImg} className="w-20 h-auto" alt="weather logo"></img>
       <h1 className="text-2xl text-customGray">Weather</h1>
     </div>
   );
--- src/components/WeatherImage.jsx
+++ src/components/WeatherImage.jsx
@@ -9,17 +9,21 @@ function WeatherImage({ id }) {
   return (
     <>
       {id < 300 ? (
-        <img src={thunderstormImg} className="w-24 h-auto"></img>
+        <img
+          src={thunderstormImg}
+          className="w-24 h-auto"
+          alt="lightning image"
+        ></img>
       ) : id < 600 ? (
-        <img src={rainImg} className="w-24 h-auto"></img>
+        <img src={rainImg} className="w-24 h-auto" alt="rain image"></img>
       ) : id < 700 ? (
-        <img src={snowImg} className="w-24 h-auto"></img>
+        <img src={snowImg} className="w-24 h-auto" alt="snow image"></img>
       ) : id < 800 ? (
-        <img src={mistImg} className="w-24 h-auto"></img>
+        <img src={mistImg} className="w-24 h-auto" alt="water image"></img>
       ) : id === 800 ? (
-        <img src={clearImg} className="w-24 h-auto"></img>
+        <img src={clearImg} className="w-24 h-auto" alt="sun image"></img>
       ) : id > 800 ? (
-        <img src={cloudsImg} className="w-24 h-auto"></img>
+        <img src={cloudsImg} className="w-24 h-auto" alt="cloud image"></img>
       ) : (
         <></>
       )}
